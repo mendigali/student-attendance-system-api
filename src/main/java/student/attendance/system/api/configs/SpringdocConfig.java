8c527f6a8217fcd0fc5c2c04918bd9ff9dcab610
mendigali <mendigalitemir13@gmail.com>
2023-12-10 03:46:14 +0600

    change swagger ui basic auth

---
+++ src/main/java/student/attendance/system/api/configs/SpringdocConfig.java
@@ -0,0 +1,12 @@
+package student.attendance.system.api.configs;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP,
+        name = "basicAuth",
+        scheme = "basic")
+public class SpringdocConfig {}
--- src/main/java/student/attendance/system/api/controllers/ScheduleController.java
+++ src/main/java/student/attendance/system/api/controllers/ScheduleController.java
@@ -1,6 +1,7 @@
 package student.attendance.system.api.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import student.attendance.system.api.models.responses.ScheduleAllResponse;
@@ -15,12 +16,14 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+    @SecurityRequirement(name = "basicAuth")
     @Operation(summary = "Get schedule for week")
     @GetMapping("/week")
     public List<ScheduleAllResponse> getScheduleWeek(@RequestParam Integer groupId) {
         return scheduleService.getScheduleWeek(groupId);
     }
 
+    @SecurityRequirement(name = "basicAuth")
     @Operation(summary = "Get schedule for today")
     @GetMapping("/today")
     public List<ScheduleAllResponse> getScheduleToday(@RequestParam Integer groupId) {
